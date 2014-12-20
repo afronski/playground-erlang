8997a1496dd942bac9c499ec7b60d417c4c4e425
Wojciech Gawronski <afronski@gmail.com>
2014-12-20 19:33:19 +0100

    KV Example - Bucket implementation.

---
+++ elixir/kv/mix.exs
@@ -0,0 +1,20 @@
+defmodule KV.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :kv,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      deps: deps
+    ]
+  end
+
+  def application do
+    [ applications: [ :logger ] ]
+  end
+
+  defp deps do
+    []
+  end
+end
