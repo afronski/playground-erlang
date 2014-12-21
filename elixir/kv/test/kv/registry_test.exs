55cbc7ee07f1876ba43939f07c412af7006a6c79
Wojciech Gawronski <afronski@gmail.com>
2014-12-21 11:06:00 +0100

    KV Example - Adding sending events with use 'GenEvent' behaviour.

--- elixir/kv/test/kv/registry_test.exs
+++ elixir/kv/test/kv/registry_test.exs
@@ -1,8 +1,21 @@
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
+  defmodule Forwarder do
+    use GenEvent
+
+    def handle_event(event, parent) do
+      send parent, event
+      { :ok, parent }
+    end
+  end
+
   setup do
-    { :ok, registry } = KV.Registry.start_link
+    { :ok, manager } = GenEvent.start_link
+    { :ok, registry } = KV.Registry.start_link(manager)
+
+    GenEvent.add_mon_handler(manager, Forwarder, self())
+
     { :ok, registry: registry }
   end
 
@@ -23,5 +36,15 @@ defmodule KV.RegistryTest do
     Agent.stop(bucket)
 
     assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "sends events on create and crash", %{ registry: registry } do
+    KV.Registry.create(registry, "shopping")
+    { :ok, bucket } = KV.Registry.lookup(registry, "shopping")
+
+    assert_receive { :create, "shopping", bucket }
+
+    Agent.stop(bucket)
+    assert_receive { :exit, "shopping", bucket }
   end
 end
