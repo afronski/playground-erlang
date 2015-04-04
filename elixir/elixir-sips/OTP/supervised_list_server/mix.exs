0c48a016f7bbae8b7024e73f76e3c8663bce0bca
Wojciech Gawronski <afronski@gmail.com>
2015-04-04 14:48:59 +0200

    Adding support for releases to the Elixir project (`:pogo` and `:relex`).

--- elixir/elixir-sips/OTP/supervised_list_server/mix.exs
+++ elixir/elixir-sips/OTP/supervised_list_server/mix.exs
@@ -1,3 +1,16 @@
+Code.append_path "_build/shared/lib/relex/ebin"
+Code.append_path "_build/shared/lib/pogo/ebin"
+
+if Code.ensure_loaded?(Relex.Release) do
+  defmodule SupervisedListServer.Release do
+    use Relex.Release
+    use Pogo.Release
+
+    def name, do: "supervised_list_server"
+    def applications, do: [:pogo, :supervised_list_server]
+  end
+end
+
 defmodule SupervisedListServer.Mixfile do
   use Mix.Project
 
@@ -5,7 +18,9 @@ defmodule SupervisedListServer.Mixfile do
     [app: :supervised_list_server,
      version: "0.0.1",
      elixir: "~> 1.0",
-     deps: deps]
+     deps: deps,
+     release: SupervisedListServer.Release
+    ]
   end
 
   def application do
@@ -17,7 +32,9 @@ defmodule SupervisedListServer.Mixfile do
 
   defp deps do
     [
-        {:exlager, github: "khia/exlager"}
+        {:exlager, github: "khia/exlager"},
+        {:relex, github: "interline/relex", branch: "start_clean"},
+        {:pogo, github: "onkel-dirtus/pogo"}
     ]
   end
 end
