d5fb85a973d8ebc1ead48dfeedc602f2cd50a22a
Wojciech Gawronski <afronski@gmail.com>
2015-04-17 09:47:28 +0200

    Code coverage in Elixir (with built-in and additional tools).

--- elixir/elixir-sips/tools/pcm_playground/mix.exs
+++ elixir/elixir-sips/tools/pcm_playground/mix.exs
@@ -4,27 +4,18 @@ defmodule PcmPlayground.Mixfile do
   def project do
     [app: :pcm_playground,
      version: "0.0.1",
+     test_coverage: [tool: Coverex.Task],
      elixir: "~> 1.0",
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+        {:coverex, "~> 1.2.0"}
+    ]
   end
 end
