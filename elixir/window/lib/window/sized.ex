2425d34fbbde01ff6e2818d45b2acdc9ca173ffb
Wojciech Gawronski <afronski@gmail.com>
2015-05-29 23:12:36 +0200

    Implementing sliding windows kata.
    - I've used protocols to define common interface for both types of
      sliding windows.

--- elixir/window/lib/window/sized.ex
+++ elixir/window/lib/window/sized.ex
@@ -1,2 +1,18 @@
 defmodule Window.Sized do
+  defstruct size: nil, items: :queue.new()
+end
+
+defimpl Windowable, for: Window.Sized do
+  def add(window = %Window.Sized{size: size, items: items}, item) do
+    if :queue.len(items) == size do
+      {_, q} = :queue.out_r(items)
+      %{ window | items: :queue.in_r(item, q)}
+    else
+      %{ window | items: :queue.in_r(item, items)}
+    end
+  end
+
+  def items(window) do
+    :queue.to_list(window.items)
+  end
 end
